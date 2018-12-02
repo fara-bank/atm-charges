bd002b1eec6ecccff3081d6be322dc0e1e80d4f6
N.Abdolalizadeh <alizadeh85a@yahoo.com>
2018-12-02 12:49:43 +0330

    New method for create report instance from a line

--- src/util/ReportFileUtil.java
+++ src/util/ReportFileUtil.java
@@ -44,40 +44,11 @@ public class ReportFileUtil {
 
             //Read File Line By Line
             while ((strLine = br.readLine()) != null) {
+
                 // Print the content on the console
                 System.out.println(strLine);
 
-                String[] fields = strLine.split("\\|");
-
-                AcqReport report = new AcqReport();
-
-                report.setRowNo(fields[0]);
-                report.setProcessType(fields[1]);
-                report.setTransactionDate(fields[2]);
-                report.setTransactionTime(fields[3]);
-                report.setStan(fields[4]);
-                report.setCardNumber(fields[5]);
-                report.setIssuerCode(fields[6]);
-                report.setTerminalNo(fields[7]);
-                report.setAmount(fields[8]);
-                report.setDeviceType(fields[9]);
-                report.setCharge(fields[10]);
-                report.setExteraInfo1(fields[11]);
-                report.setFunctionCode(fields[12]);
-                report.setLedgerDate(fields[13]);
-                report.setLedgerNoShetab(fields[14]);
-                report.setIssuerCcyAmount(fields[15]);
-                report.setIssuerCcy(fields[16]);
-                report.setExtraInfo2(fields[17]);
-                report.setExtraInfo3(fields[18]);
-                report.setExtraInfo4(fields[19]);
-                report.setExtraInfo5(fields[20]);
-                report.setExtraInfo6(fields[21]);
-                report.setExtraInfo7(fields[22]);
-                report.setExtraInfo8(fields[23]);
-                report.setFileType(file.getName().substring(file.getName().indexOf('.')));
-                report.setUserId("SYS");
-
+                AcqReport report = valueOf(strLine, file.getName().substring(file.getName().indexOf('.')));
                 dao.save(report);
             }
 
@@ -98,5 +69,40 @@ public class ReportFileUtil {
             if (dao != null)
                 dao.close();
         }
+    }
+
+    private AcqReport valueOf(String strLine, String filename) {
+        String[] fields = strLine.split("\\|");
+
+        AcqReport report = new AcqReport();
+
+        report.setRowNo(fields[0]);
+        report.setProcessType(fields[1]);
+        report.setTransactionDate(fields[2]);
+        report.setTransactionTime(fields[3]);
+        report.setStan(fields[4]);
+        report.setCardNumber(fields[5]);
+        report.setIssuerCode(fields[6]);
+        report.setTerminalNo(fields[7]);
+        report.setAmount(fields[8]);
+        report.setDeviceType(fields[9]);
+        report.setCharge(fields[10]);
+        report.setExteraInfo1(fields[11]);
+        report.setFunctionCode(fields[12]);
+        report.setLedgerDate(fields[13]);
+        report.setLedgerNoShetab(fields[14]);
+        report.setIssuerCcyAmount(fields[15]);
+        report.setIssuerCcy(fields[16]);
+        report.setExtraInfo2(fields[17]);
+        report.setExtraInfo3(fields[18]);
+        report.setExtraInfo4(fields[19]);
+        report.setExtraInfo5(fields[20]);
+        report.setExtraInfo6(fields[21]);
+        report.setExtraInfo7(fields[22]);
+        report.setExtraInfo8(fields[23]);
+        report.setFileType(filename);
+        report.setUserId("SYS");
+
+        return report;
     }
 }
