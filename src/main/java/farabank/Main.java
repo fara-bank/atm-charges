ff5b33c2640ff7dc296c6f919ce0b701f8e01b67
Atieh Jahanshad (DSA) <A.Jahanshad@dsa.com>
2019-06-16 13:08:42 +0430

    Upgrade the project structure to maven

--- src/main/java/farabank/Main.java
+++ src/main/java/farabank/Main.java
@@ -1,8 +1,10 @@
+package farabank;
+
 import util.ReportFileUtil;
 
 public class Main {
     public static void main(final String[] args) throws Exception {
         ReportFileUtil reportFileUtil = new ReportFileUtil();
-        reportFileUtil.saveAllRow();
+        // reportFileUtil.saveAllRow();
     }
 }
